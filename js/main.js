4bbf3b0952d8cc2939fb52f1d2fe56ddb72252ff
srinathunta <srinath.unta@gmail.com>
2022-06-30 12:27:41 +0530

    first commit

--- js/main.js
+++ js/main.js
@@ -1,4 +1,24 @@
 $(document).ready(function () {
+  $(window).scroll(function () {
+    if ($(this).scrollTop() >= 300) {
+      // this refers to window
+      $("header").css("background", "#fff");
+      $("header nav a.menu-item, .header-btn-mutual").css("color", "#000");
+      $(".header-btn-mutual").css("border-color", "#000");
+    } else {
+      $("header").css("background", "transparent");
+      $("header nav a.menu-item, .header-btn-mutual").css("color", "#fff");
+      $(".header-btn-mutual").css("border-color", "#fff");
+    }
+  });
+
+  $(".trade-card ul li").click(function () {
+    $(".trade-card ul li").removeClass("active");
+    $(".right-content div").removeClass("active");
+    $(this).addClass("active");
+    $("#" + $(this).attr("data-id")).addClass("active");
+  });
+
   $(".hero-slider").slick({
     dots: true,
     arrows: false,
